b7542c0ad0f0924d0d10ab7ee324f036245b349b
Himanshu kumar <himanshu231204@gmail.com>
2026-02-26 13:50:44 +0530

    Add currency conversion functions: Implement get_conversion_factors and convert tools for currency conversion

--- Mini_projects/currency_conversion.py
+++ Mini_projects/currency_conversion.py
@@ -1,6 +1,28 @@
 from langchain_ollama import ChatOllama
+from langchain_community.tools import tool
+from langchain_core.tools import InjectedToolArg
+from typing import Annotated
+import requests
+
 
 llm = ChatOllama(
     model="qwen3.5:397b-cloud",  
     temperature=0.2
 )
+
+
+@tool
+def get_conversion_factors(base_currency:str, target_currency:str) -> float:
+    """ this function will convert the base currency to target currency and return the converted amount"""
+    url=f' https://v6.exchangerate-api.com/v6/437eb96292d41576c190633b/pair/{base_currency}/{target_currency}'
+    response = requests.get(url)
+    return response.json()
+
+
+@tool
+def convert(base_currency:int, conversion_rate:Annotated[float, InjectedToolArg]) -> float:
+    """ this function will convert the base currency to target currency and return the converted amount"""
+    return base_currency * conversion_rate
+
+print(convert.args)
+
