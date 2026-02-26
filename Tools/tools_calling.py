bb35579de7dfa77765c8070cd185f993c98fdc7d
Himanshu kumar <himanshu231204@gmail.com>
2026-02-26 13:27:19 +0530

    Enhance tools_calling.py: Add DuckDuckGoSearchResults import, refine query message, and implement tool call execution for LLM response generation

--- Tools/tools_calling.py
+++ Tools/tools_calling.py
@@ -1,9 +1,10 @@
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import PromptTemplate
-from langchain_core.messages import HumanMessage 
+from langchain_core.messages import HumanMessage ,ToolMessage
 from langchain_core.runnables import RunnableLambda
 from langchain_core.tools import tool 
-from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import DuckDuckGoSearchRun, DuckDuckGoSearchResults
+
 
 
 
@@ -36,7 +37,41 @@ llm_tool =llm.bind_tools([addNums, search_tool])
 
 
 
-query=HumanMessage('Impoetant discussion of Ai submit in india, please search for the news and give me a summary, and also calculate 2+3 for me')
+query=HumanMessage('Important discussion of Ai submit in india, please search for the news and give me a summary, and also calculate 2+3 for me you must use the tools I provided you')
 
 message=[query]
-print(llm_tool.invoke(message))
+
+#invoke the llm with tools
+
+response = llm_tool.invoke(message)
+print("tools calls requested by llm: ",response.tool_calls)
+
+# execute the tool calls and get the results
+
+tool_message=[] # to store the tool calls and results
+
+for call in response.tool_calls:
+    tool_name=call["name"]
+    tool_args=call["args"]
+    tool_id=call["id"]
+    
+    if tool_name.startswith("addNums"):
+        result=addNums.invoke(tool_args)
+    elif tool_name.startswith("DuckDuckGoSearchResults"):
+        result=search_tool.invoke(tool_args)
+    else:
+        result="tool not found"
+    tool_message.append(
+        ToolMessage(
+            content=str(result), 
+            tool_call_id=tool_id
+            )
+        )
+    
+    
+    
+ #send the tool results back to llm for final response generation
+ 
+final_response=llm_tool.invoke(message+[response]+tool_message)  
+
+print("final response from llm:\n ",final_response.content) 
